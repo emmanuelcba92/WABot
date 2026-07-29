1c2fd353da04fd4887f4762a5f653ab96c485d62
Emmanuel <emmanuel.ag92@gmail.com>
2026-07-29 18:56:18 -0300

    Auto-install dependencies in Iniciar_Bot.bat if node_modules is missing on a new PC

--- Iniciar_Bot.bat
+++ Iniciar_Bot.bat
@@ -6,5 +6,13 @@ echo   INICIANDO CONECTOR DE WHATSAPP WEB - CLÍNICA MÉDICA
 echo ============================================================
 echo.
 cd /d "%~dp0gateway"
+
+if not exist "node_modules\" (
+    echo [!] Instalando dependencias necesarias por primera vez...
+    call npm.cmd install
+    echo [OK] Instalacion completada con exito.
+    echo.
+)
+
 call npm.cmd start
 pause
